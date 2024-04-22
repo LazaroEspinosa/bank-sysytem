d82c12cc0b82998dc5f3ad5c832d313309fd4756
Lazaro Espinosa <espinosa.lazaro2@gmail.com>
2024-04-22 16:17:46 -0600

    Adding notes of abstraction in Cuenta class

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,3 +1,11 @@
+/*
+ABSTRACCION: Una clase abstracta se declara despues del modificador de acceso.
+Sirve para declarar una clase padre como plantilla, por ejemplo: un vendedor y un gerente son empleados;
+por lo tanto, empleado seria la clase abstracta, mientras que gerente y vendedor serian clases que heredan (extienden) de ella.
+Las clases abstractas no pueden ser instanciadas, pero las clases hijas si.
+Las clases abstractas deben tener al menos un metodo abstracto y este debe ser sobreescrito (@override) en las clases hijas.
+Esto es para que valga la pena la intencion de crear una clase abstracta. Cada metodo abstracto sera sobreescrito.
+ */
 public abstract class Cuenta implements Autenticate{
 
     //ATRIBUTOS
