741733b49b6a3b5b955f7d4240b7cf0e713f2461
LazaroEspinosa <30704172+LazaroEspinosa@users.noreply.github.com>
2024-04-22 19:36:32 -0600

    Adding notes to CuentaGold class, about override methods due to interfaces implemented and abstract methods in parent class.

--- src/CuentaGold.java
+++ src/CuentaGold.java
@@ -1,13 +1,29 @@
 public class CuentaGold extends Cuenta{
 
+    //ATRIBUTOS
+    
     private String clave;
 
     //CONSTRUCTOR
+    /*
+    El constructor de la clase hija es obligatorio porque:
+    La clase padre tiene un constructor que no es vacio.
+    El constructor requiere parametros.
+    Ahora, como vamos a acceder a los atributos de la clase padre desde la clase hija?
+    Con el metodo super(), En conjunto con el constructor, esto puede llamar al metodo padre y proceder con su requerimiento.
+    */
+
     public CuentaGold(String cuentaId, String clienteId) {
         super(cuentaId, clienteId);
     }
 
     //METODOS
+    /*
+    El metodo comprar es sobreescrito porque es un metodo abstracto en la clase padre.
+    Los metodos setClave y logIn son sobreescritos porque son metodos de la interface Autenticate.
+    La interface Autenticate se implementa en la clase cuenta padre, que al ser abstracta no necesita sobreescribir los metodos de la interface;
+    Pero sus clases hijas si.
+    */
     @Override
     public void comprar(double valor) {
         if(retirar(valor)){
