9b6a7fd2188d1275174e066dc1a298d8b0b65241
LazaroEspinosa <30704172+LazaroEspinosa@users.noreply.github.com>
2024-04-27 23:01:36 -0600

    Adding notes for exceptions

--- src/CuentaBloqueadaException.java
+++ src/CuentaBloqueadaException.java
@@ -1,6 +1,7 @@
 /*
 EXCEPTIONS
-El control de exceptions es importante para identificar y corregir interrupciones en el flujo del codigo
+El control de exceptions es importante para identificar y corregir interrupciones en el flujo del codigo.
+El control de excepciones tambien ayuda a mostrar la causa de un error de forma mas amigable para el usuario.
 Se crea una clase que extiende de Exception, siendo una checked exception.
 La idea es lanzarla cuando alguien intente hacer su login e indicar su motivo.
 */
